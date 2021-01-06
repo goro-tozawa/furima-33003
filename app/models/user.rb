0af1a65d901c07ebfd4ff3348c415570e588a8d0
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-06 11:41:18 +0900

    バリデーション＋rspec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname,presence: true
+  validates :encrypted_password, presence: true    
+
+  validates :encrypted_password,:password,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
+
+  validates :last_name, presence: true    
+  validates :first_name, presence: true    
+  validates :last_kana, presence: true    
+  validates :last_kana, presence: true    
+  validates :first_kana, presence: true    
+  validates :birthday, presence: true   
+
+ has_many :items
 end
