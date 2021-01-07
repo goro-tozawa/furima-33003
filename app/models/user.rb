9377301fded8f40ff2848d54acbe6123031b0225
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-07 16:45:40 +0900

    ログイン　パス追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :items
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -6,20 +7,17 @@ class User < ApplicationRecord
   with_options presence: true do
 
     validates :nickname
-
     validates :encrypted_password
     
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password,:encrypted_password,with: PASSWORD_REGEX, message: "both letters and numbers"
+    validates_format_of :password,with: PASSWORD_REGEX, message: "both letters and numbers"
     
     validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Last name is invalid. Input full-width characters."}
     validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name is invalid. Input full-width characters."}
 
 
     validates :last_kana,:first_kana,format:{with: /\A[ァ-ヶー－]+\z/}
-
     validates :birthday  
 
   end
-  has_many :items
 end
