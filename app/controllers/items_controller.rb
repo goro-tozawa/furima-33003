94f352cd7991b606ab187aebae46710adc94584b
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-13 21:48:13 +0900

    商品情報編集　完成（仮）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :move_to_index]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -21,20 +23,26 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
   
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-     item = Item.find(params[:id])
-    #  item.update(item_params)
-     if item.update(item_params)
+     if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
     end
   end
 
