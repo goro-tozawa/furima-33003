b539c5ae233c579419e7c95057d221b8d22bacba
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-15 14:55:36 +0900

    orderモデルとコントローラー

---
+++ app/models/user_purchase.rb
@@ -0,0 +1,10 @@
+class UserPurchase
+  include ActiveModel::Model
+  attr_accessor :
+
+  
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :users
   resources :items
+  resources :orders, only: [:create]
+  
 end
