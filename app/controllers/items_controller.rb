38a9ba29b6f9f75680999bf6d1f47fbd9702ba51
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-20 20:57:01 +0900

    current_user削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,7 @@ class ItemsController < ApplicationController
   end
   
   def edit
-    if @item.purchase.present? && current_user
+    if @item.purchase.present? 
       redirect_to root_path
     end
   end
