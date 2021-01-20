617486e0a3205d591f7f684fa2d24168d67b2321
goro-tozawa <ralugoro.0905@icloud.com>
2021-01-20 16:28:16 +0900

    番地＋建物名を追加

--- spec/factories/user_purchase.rb
+++ spec/factories/user_purchase.rb
@@ -3,8 +3,10 @@ FactoryBot.define do
     postal_code                                    {"111-1111"}
     area_id                                             {"2"}
     municipality                                     {"横浜市"}
-    address                                             {"2-54-41"}
+    address                                             {"test2-54-41"}
     phone_number                                {12345678910}
+    building_name                                  {"test"}
     token {"tok_abcdefghijk00000000000000000"}
+  
    end
  end
